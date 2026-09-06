af7e7e7786dac6dab0aaeea070b858bf18a20049
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-09-06 20:08:43 +0600

    style: normalize atomic interfaces

--- src/Cache/Adapter/AtomicCachePoolInterface.php
+++ src/Cache/Adapter/AtomicCachePoolInterface.php
@@ -13,7 +13,7 @@ use Psr\Cache\CacheItemInterface;
  */
 interface AtomicCachePoolInterface extends InternalCachePoolInterface
 {
-    public function atomicSetIfAbsent(CacheItemInterface $item): bool;
-
     public function atomicGetAndDelete(string $key): CacheItemInterface;
+
+    public function atomicSetIfAbsent(CacheItemInterface $item): bool;
 }
